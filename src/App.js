f563c2d38785e57ccb1cee4b20837cfd8780c72f
Faraz Mahmood <faraz.m4765@gmail.com>
2023-09-16 23:06:04 +0500

    working with the design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Body/Header/Navbar";
 import ImageUploader from "./Components/Pages/ImagesUploader/ImageUploader";
 
+
 function App() {
   return (
     <div >
--- src/Components/Pages/ImagesUploader/ImageUploader.jsx
+++ src/Components/Pages/ImagesUploader/ImageUploader.jsx
@@ -1,27 +1,18 @@
-
-
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { PDFDocument, rgb } from 'pdf-lib';
-// import { PDFDocument, rgb } from 'pdf-lib/build/pdf-lib';
-
-
-
+import { PDFDocument } from 'pdf-lib';
+import Draggable from 'react-draggable';
+import SignatureCanvas from 'react-signature-canvas';
+import './styles.css'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function PdfViewer() {
-    const [selectedAnnotation, setSelectedAnnotation] = useState(null);
-
     const [pdfFile, setPdfFile] = useState(null);
     const [numPages, setNumPages] = useState(null);
-    const [annotations, setAnnotations] = useState([
-        {
-            text: 'Sarib Bhai',
-            x: 100, // Initial X position
-            y: 100, // Initial Y position
-        },
-        // Add more annotations as needed
-    ]);
+    const [signatureDataUrl, setSignatureDataUrl] = useState('');
+    const [signaturePosition, setSignaturePosition] = useState({ x: 50, y: 100 });
+
+    const signatureRef = useRef(null);
 
     const onFileChange = (e) => {
         const file = e.target.files[0];
@@ -36,150 +27,50 @@ function PdfViewer() {
         setNumPages(numPages);
     };
 
-    const handlePdfClick = (e) => {
-        const clickedX = e.nativeEvent.offsetX;
-        const clickedY = e.nativeEvent.offsetY;
-
-        // Check if an annotation was clicked
-        const clickedAnnotation = annotations.find((annotation) => {
-            const { x, y, text } = annotation;
-            const width = text.length * 10;
-            const height = 20;
-
-            return (
-                clickedX >= x &&
-                clickedX <= x + width &&
-                clickedY >= y &&
-                clickedY <= y + height
-            );
-        });
-
-        if (clickedAnnotation) {
-            // If an annotation was clicked, select it
-            setSelectedAnnotation(clickedAnnotation);
-        } else if (selectedAnnotation) {
-            // If an annotation is selected, move it to the clicked position
-            const updatedAnnotations = annotations.map((annotation) =>
-                annotation === selectedAnnotation
-                    ? { ...annotation, x: clickedX, y: clickedY }
-                    : annotation
-            );
-            setAnnotations(updatedAnnotations);
-            setSelectedAnnotation(null); // Deselect after moving
-        }
+    const handleClear = () => {
+        signatureRef.current.clear();
+        setSignatureDataUrl('');
     };
 
-
-    // const generateUpdatedPdf = async () => {
-    //     try {
-    //         // Load the existing PDF
-    //         const existingPdfBytes = await pdfFile.arrayBuffer();
-    //         const pdfDoc = await PDFDocument.load(existingPdfBytes);
-
-    //         // Get the first page of the PDF
-    //         const pages = pdfDoc.getPages();
-    //         const page = pages[0]; // Assuming you're adding annotations to the first page
-
-    //         // Add text annotations
-    //         annotations.forEach((annotation) => {
-    //             const { x, y, text } = annotation;
-
-    //             // Create a new text field
-    //             const textWidth = text.length * 10; // Adjust this as needed
-    //             const textHeight = 20; // Adjust this as needed
-    //             const textAnnotation = page.drawText(text, {
-    //                 x,
-    //                 y: page.getHeight() - y - textHeight, // Invert Y-coordinate
-    //                 size: 12,
-    //                 color: rgb(0, 0, 0), // Black color
-    //             });
-
-    //             // Modify text appearance if needed
-    //             // textAnnotation.setFontSize(12);
-
-    //             // Add more styling options here if necessary
-
-    //             // Add the text annotation to the page
-    //             page.drawRectangle({
-    //                 x,
-    //                 y: page.getHeight() - y - textHeight,
-    //                 width: textWidth,
-    //                 height: textHeight, 
-    //                 borderColor: rgb(0, 0, 0), // Black border
-    //                 borderWidth: 1,
-    //             });
-
-    //             page.drawText(text, {
-    //                 x,
-    //                 y: page.getHeight() - y - textHeight + 5, // Adjust vertical position
-    //                 size: 12,
-    //                 color: rgb(0, 0, 0), // Black color
-    //             });
-    //         });
-
-    //         // Serialize the modified PDF
-    //         const modifiedPdfBytes = await pdfDoc.save();
-
-    //         // Create a Blob from the PDF bytes
-    //         const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
-
-    //         // Create a download link and trigger the download
-    //         const downloadLink = document.createElement('a');
-    //         downloadLink.href = URL.createObjectURL(blob);
-    //         downloadLink.download = 'updated_pdf.pdf';
-    //         downloadLink.click();
-    //     } catch (error) {
-    //         console.error('Error generating updated PDF:', error);
-    //     }
-    // };
-
-
+    const handleGenerate = () => {
+        const dataUrl = signatureRef.current.toDataURL('image/png', { transparent: true });
+        setSignatureDataUrl(dataUrl);
+    };
 
     const generateUpdatedPdf = async () => {
         try {
+            if (!pdfFile) {
+                alert('Please upload a PDF file.');
+                return;
+            }
+
             // Load the existing PDF
             const existingPdfBytes = await pdfFile.arrayBuffer();
             const pdfDoc = await PDFDocument.load(existingPdfBytes);
-    
+
             // Get the first page of the PDF
             const pages = pdfDoc.getPages();
             const page = pages[0]; // Assuming you're adding annotations to the first page
-    
-            // Specify the font for text annotations
-            const font = await pdfDoc.embedFont(PDFLib.StandardFonts.Helvetica);
-    
-            // Add text annotations
-            annotations.forEach((annotation) => {
-                const { x, y, text } = annotation;
-    
-                // Create a new text field
-                const textWidth = font.widthOfTextAtSize(text, 12); // Calculate text width
-                const textHeight = 12; // Font size
-                const textAnnotation = page.drawText(text, {
-                    x,
-                    y: page.getHeight() - y - textHeight, // Invert Y-coordinate
-                    size: 12,
-                    font,
-                    color: rgb(0, 0, 0), // Black color
+
+            // Add the signature image to the page
+            if (signatureDataUrl) {
+                const imageBytes = await fetch(signatureDataUrl).then((res) => res.arrayBuffer());
+                const image = await pdfDoc.embedPng(imageBytes);
+                const { width, height } = image.size();
+                page.drawImage(image, {
+                    x: signaturePosition.x,
+                    y: page.getSize().height - signaturePosition.y - height, // Invert Y-axis
+                    width,
+                    height,
                 });
-    
-                // Add the text annotation to the page
-                page.drawRectangle({
-                    x,
-                    y: page.getHeight() - y - textHeight,
-                    width: textWidth,
-                    height: textHeight, 
-                    borderColor: rgb(0, 0, 0), // Black border
-                    borderWidth: 1,
-                });
-            });
-    
+            }
+
             // Serialize the modified PDF
             const modifiedPdfBytes = await pdfDoc.save();
-    
+
             // Create a Blob from the PDF bytes
             const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
-    
+
             // Create a download link and trigger the download
             const downloadLink = document.createElement('a');
             downloadLink.href = URL.createObjectURL(blob);
@@ -189,39 +80,74 @@ function PdfViewer() {
             console.error('Error generating updated PDF:', error);
         }
     };
-    
+
     return (
         <div className='container mt-5'>
             <div>
                 <label htmlFor="formFileLg" className="form-label">Upload a PDF file</label>
                 <input className="form-control form-control-lg" id="formFileLg" type="file" accept="application/pdf" onChange={onFileChange} />
             </div>
+
+            <div>
+                <div className="file-upload">
+                    <button className="file-upload-btn" id="formFileLg" type="file" accept="application/pdf" onChange={onFileChange}>Add Image</button>
+                    <div className="image-upload-wrap">
+                        <input className="file-upload-input" type="file" onchange="readURL(this);" accept="image/*" />
+                        <div className="drag-text">
+                            <h3>Drag and drop a file or select add Image</h3>
+                        </div>
+                    </div>
+                    <div className="file-upload-content">
+                        <img className="file-upload-image" src="#" alt="your image" />
+                        <div className="image-title-wrap">
+                            <button type="button" onclick="removeUpload()" className="remove-image">Remove <span className="image-title">Uploaded Image</span></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div>
+                <div style={{ border: "2px solid black", width: 500, height: 200 }}>
+                    <SignatureCanvas
+                        canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }}
+                        ref={signatureRef}
+                    />
+                </div>
+
+                <br></br>
+                <button style={{ height: "30px", width: "60px" }} onClick={handleClear}>Clear</button>
+                <button style={{ height: "30px", width: "60px" }} onClick={handleGenerate}>Save</button>
+
+                <br /><br />
+
+                <Draggable
+                    onDrag={(e, { x, y }) => setSignaturePosition({ x, y })}
+                    position={signaturePosition}
+                >
+                    <img src={signatureDataUrl} alt="Signature" />
+                </Draggable>
+            </div>
+
             {pdfFile && (
                 <div>
                     <Document
                         file={pdfFile}
                         onLoadSuccess={onDocumentLoadSuccess}
-                        onClick={handlePdfClick}
                     >
                         {Array.from(new Array(numPages), (el, index) => (
                             <Page key={`page_${index + 1}`} pageNumber={index + 1}>
-                                {annotations.map((annotation, i) => (
+                                {signatureDataUrl && (
                                     <div
-                                        key={`annotation_${i}`}
                                         style={{
                                             position: 'absolute',
-                                            left: annotation.x,
-                                            top: annotation.y,
-                                            cursor: 'move',
-                                            border: '1px solid #000',
+                                            left: signaturePosition.x,
+                                            top: signaturePosition.y,
                                         }}
-                                        className={`homemade-font ${annotation === selectedAnnotation ? 'selected' : ''
-                                            }`}
-                                        onMouseDown={() => setSelectedAnnotation(annotation)}
                                     >
-                                        {annotation.text}
+                                        <img src={signatureDataUrl} alt="Signature" />
                                     </div>
-                                ))}
+                                )}
                             </Page>
                         ))}
                     </Document>
@@ -229,7 +155,6 @@ function PdfViewer() {
             )}
 
             <button onClick={generateUpdatedPdf}>Download Updated PDF</button>
-
         </div>
     );
 }
